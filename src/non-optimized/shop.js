e7308e6d3dc16c2b94bd452617c8ec87365adfbf
Monicacholico <monicachol@hotmail.com>
2020-09-14 16:34:37 -0500

    finished chapter 519

--- src/non-optimized/shop.js
+++ src/non-optimized/shop.js
@@ -1,4 +1,21 @@
-import { initProducts, addProduct } from './product-management';
+import { products } from './products';
+import { renderProducts } from './rendering'
+
+function addProduct(event) {
+    import('./product-management.js').then(mod => {
+        mod.addProduct(event);
+    })
+}
+
+function deleteProduct(productId) {
+    import('./product-management.js').then(mod => {
+        mod.deleteProduct(productId);
+    })
+}
+
+function initProducts() {
+    renderProducts(products, deleteProduct);
+  }
 
 const addProductForm = document.getElementById('new-product');
 
